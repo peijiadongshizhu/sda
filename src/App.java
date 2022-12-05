48d3e1998c5902e73a10cc1e90a5fe55c01ca372
wangjianqianh <1015400785@qq.com>
2022-12-05 14:57:09 +0800

    sdasd

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/**
+ * @description: TODO
+ * @author: 王建强
+ * @date: 2022/12/5 14:56
+ * @version: v1.0
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
